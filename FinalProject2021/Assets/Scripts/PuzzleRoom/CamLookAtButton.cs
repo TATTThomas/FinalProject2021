2936270d7ce6062d5ce1e78dab460acc42418bd2
brian5k6 <96015@tzps.tc.edu.tw>
2021-12-30 02:28:13 +0800

    123

---
+++ FinalProject2021/Assets/Scripts/PuzzleRoom/CamLookAtButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamLookAtButton : MonoBehaviour
+{
+    public float maxDistance;
+    public RotateRoad rr;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.x - transform.position.x < 1 && player.transform.position.x - transform.position.x > -1 && player.transform.position.z - transform.position.z > -1 && player.transform.position.z - transform.position.z < 1 && player.transform.position.y - transform.position.y > -1 && player.transform.position.y - transform.position.y < 1)
+        {
+            Debug.Log("r");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                rr.Rotate_road();
+            }
+        }
+    }
+    
+}
