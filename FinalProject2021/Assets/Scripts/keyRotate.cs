c1d197fe124a882242bcc1382e3d7a208c682c0b
Willtp6 <will910623@gmail.com>
2021-12-31 05:20:52 +0800

    laserRoom Key

--- FinalProject2021/Assets/Scripts/keyRotate.cs
+++ FinalProject2021/Assets/Scripts/keyRotate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rotate : MonoBehaviour
+public class keyRotate : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0.5f, 1, 0.5f);
     }
 }
